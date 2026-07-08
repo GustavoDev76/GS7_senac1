c1a76361732d3d69ebef6ec7b003986595194f97
GustavoDev76 <gustmenezes66@gmail.com>
2026-07-08 20:32:15 -0300

    aprendendo (FOR)

---
+++ prog31.py
@@ -0,0 +1,10 @@
+#b = 0
+#or i in range (1,6):
+#    b = b + i
+#    print(b)
+
+m = 0
+for i in range (0,5):
+    x = int(input("digite um valor: "))
+    m = m + x
+print(f"o valor de m é {m}")
---
+++ prog32.py
@@ -0,0 +1,4 @@
+tabuada = int(input("digite um valor: "))
+for i in range (1, 11):
+    t = tabuada * i
+    print(f"{tabuada} X {i} = {t}")
