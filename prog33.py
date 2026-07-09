620985223f30e770608831212e5eea7674c82b39
GustavoDev76 <gustmenezes66@gmail.com>
2026-07-08 21:26:49 -0300

    aprendendo (FOR)

---
+++ prog33.py
@@ -0,0 +1,12 @@
+#x = 5
+#y = x % 2
+#print(y)
+
+numero= int(input("digite um valor: "))
+
+valor = numero % 2
+
+if valor != 0:
+    print("IMPAR")
+else:
+    print("PAR")
---
+++ prog34.py
@@ -0,0 +1,8 @@
+for i in range (5):
+    numero = int(input("digite um valor: "))
+    valor = numero % 2
+    if valor == 1:
+        print(f"{numero} é impar")
+    else:
+        print(f"{numero} é par")
+
