1435b5890d627e51a2678a10c6dcb9b89147fe75
GustavoDev76 <gustmenezes66@gmail.com>
2026-07-10 21:45:18 -0300

    primeiro commit

--- prog39.py
+++ prog39.py
@@ -1,7 +1,10 @@
 carros = {}
 for i in range (2):
-    carro = str(input("Qual Carro: "))
-    marca = str(input("Qual Marca do Carro: "))
+    carro = input("Qual Carro: ")
+    marca = input("Qual Marca do Carro: ")
     valor = float(input("valor do carro: "))
-    carros [carro, marca] = valor
+    carros[carro] = {
+        "marca": marca,
+        "valor": valor
+    }
     print(f"Informacoes Do Carro = {carros} ")
