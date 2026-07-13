e6058dc9f3d8bf424a61876346a86b7597caa972
GustavoDev76 <gustmenezes66@gmail.com>
2026-07-13 20:38:03 -0300

    exercicios while

---
+++ teste.py
@@ -0,0 +1,11 @@
+# USANOD (FOR)
+numero = int(input("digite um numero: "))
+for i in range (1, 10):
+    print(f"{numero} X {i} = {numero * i}")
+
+# USANDO (WHILE)
+valor = int(input("digite um valor: "))
+contador = 0
+while contador <= 10:
+    contador += 1
+    print(f"{valor} X {contador} = {valor * contador}")    
